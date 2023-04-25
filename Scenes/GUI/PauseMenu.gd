bd31ab527fc829218c59b0b85e550ee133bdb2d8
Yeetcoeschann <jlewis.cw743@gmail.com>
2023-04-25 00:30:01 -0400

    More Tweaks
    - Portal must be activated
    - Pointer wont show up when unpaused
    - Some level 1 tweaks
    - Removed unused tilesets
    
    Courtesy of Brendan's playtest

--- Scenes/GUI/PauseMenu.gd
+++ Scenes/GUI/PauseMenu.gd
@@ -2,6 +2,10 @@ extends Control
 
 var is_paused = false : set = set_is_paused
 
+func _process(delta):
+	if is_paused == false:
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
 		self.is_paused = !is_paused
@@ -14,6 +18,8 @@ func set_is_paused(value):
 	visible = is_paused
 	if visible == true:
 		$CenterContainer/VBoxContainer/ResumeButton.grab_focus()
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 
 func _on_resume_button_pressed():
--- Scenes/Interactables/Portal/portal.gd
+++ Scenes/Interactables/Portal/portal.gd
@@ -3,10 +3,16 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("default")
+	$upIcon.visible = false
 	
-func _on_body_entered(body):
-	if body.is_in_group("player"):
-		if get_tree().current_scene.name == "TestLevel":
-			get_tree().change_scene_to_file("res://Scenes/Maps/level_1/level_1.tscn")
-		else:
-			get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")
+
+func _process(delta):
+	if self.overlaps_body(get_tree().get_first_node_in_group("player")):
+		$upIcon.visible = true
+		if Input.is_action_pressed("up"):
+			if get_tree().current_scene.name == "TestLevel":
+				get_tree().change_scene_to_file("res://Scenes/Maps/level_1/level_1.tscn")
+			else:
+				get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")
+	else:
+		$upIcon.visible = false
